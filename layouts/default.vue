1bf84ebb84d2c443fff3857f25899ba450180a9d
Sebastien Chopin <sebastien.chopin@neo9.fr>
2017-08-30 15:58:47 +0200

    OK

---
+++ layouts/default.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+@font-face {
+  font-family: Interface;
+  src:
+    url("~static/fonts/Interface-Regular.eot?#iefix") format("embedded-opentype"),
+    url("~static/fonts/Interface-Regular.woff2") format("woff2"),
+    url("~static/fonts/Interface-Regular.woff") format("woff"),
+    url("~static/fonts/Interface-Regular.ttf") format("truetype");
+}
+
+html {
+  font-family: Interface, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
